98e03ee7ae61cec15f9718bb3a663ecc8b0ebca1
19khaled88 <kkhaled88hasan@gmail.com>
2025-11-20 23:20:51 +0600

    worked on controller code modifications

--- src/controller/blog.ts
+++ src/controller/blog.ts
@@ -4,72 +4,108 @@ import TryCatch from "../utils/TryCatch.js";
 import { getCache, setCahce } from "../redis/redis.cache.js";
 
 
-export const getAllBlogs = TryCatch(async(req,res)=>{
-    const {searchQuery = "", category = ""} = req.query;
-    
+export const getAllBlogs = TryCatch(async (req, res) => {
+    const { searchQuery = "", category = "" } = req.query;
+
+
+    // --------------------------
+    // CHECK REDIS CACHE
+    // --------------------------
     const cacheKey = `blogs:${searchQuery}:${category}`;
     const cached = await getCache(cacheKey)
 
-    if(cached){
+    if (cached) {
         console.log("Serving from redis cache");
-        res.json(cached);
-        return;
+        return res.status(200).json({
+            success: true,
+            message: "Blogs fetched successfully (cache)",
+            data: cached
+        });
     }
+
+    // --------------------------
+    // BUILD QUERY
+    // --------------------------
     let blogs;
 
-    if(searchQuery && category){
-        blogs = await sql`SELECT * FROM blogs WHERE (title ILIKE ${
-            '%' + searchQuery + '%'
-        } OR description ILIKE ${
-            '%' + searchQuery + '%' 
-        }) AND category = ${category} ORDER BY created_at DESC`;
-    }else if(searchQuery){
-        blogs = await sql`SELECT * FROM blogs WHERE (title ILIKE ${
-            '%' + searchQuery + '%'
-        } OR description ILIKE ${
-            '%' + searchQuery + '%' 
-        }) ORDER BY created_at DESC`;
-    } else if(category){
+    if (searchQuery && category) {
+        blogs = await sql`SELECT * FROM blogs WHERE (title ILIKE ${'%' + searchQuery + '%'
+            } OR description ILIKE ${'%' + searchQuery + '%'
+            }) AND category = ${category} ORDER BY created_at DESC`;
+    } else if (searchQuery) {
+        blogs = await sql`SELECT * FROM blogs WHERE (title ILIKE ${'%' + searchQuery + '%'
+            } OR description ILIKE ${'%' + searchQuery + '%'
+            }) ORDER BY created_at DESC`;
+    } else if (category) {
         blogs = await sql`SELECT * FROM blogs WHERE category =${category} ORDER BY created_at DESC`;
     } else {
         blogs = await sql`SELECT * FROM blogs ORDER BY created_at DESC`;
     }
 
     console.log('Serviing from db')
-    await setCahce(cacheKey, blogs,3600)
 
-    res.status(200).json({
-        message:"Blogs fetched successfully",
-        data:blogs
+
+    // --------------------------
+    // STORE IN CACHE FOR 1 HOUR
+    // --------------------------
+    await setCahce(cacheKey, blogs, 3600)
+
+    return res.status(200).json({
+        success: true,
+        message: "Blogs fetched successfully",
+        data: blogs
     });
-});   
+});
 
 
-export const getSingleBlog = TryCatch(async(req,res)=>{
-    const {id} = req.params;    
+export const getSingleBlog = TryCatch(async (req, res) => {
+    const { id } = req.params;
 
+    // --------------------------
+    // CHECK REDIS CACHE
+    // --------------------------
     const cacheKey = `blog:${id}`
     const cached = await getCache(cacheKey);
 
-    if(cached){
+    if (cached) {
         console.log('Serviing from Redis cache');
-        res.json(cached);
-        return;
+        return res.status(200).json({
+            success: true,
+            message: 'Blog fetched successfully (cache)',
+            ...cached
+        });
+
     }
 
+    // --------------------------
+    // FETCH BLOG FROM DATABASE
+    // --------------------------
     const blog = await sql`SELECT * FROM blogs WHERE id = ${id};`;
-    if(blog.length === 0){
-        return res.status(404).json({message:"Blog not found"});
+    if (blog.length === 0) {
+        return res.status(404).json({
+            success: false,
+            message: "Blog not found"
+        });
     }
-   
 
-    if(!blog[0]){
-        return res.status(404).json({message:"Blog not found"});
+
+    if (!blog[0]) {
+        return res.status(404).json({
+            success: false,
+            message: "Blog not found"
+        });
     }
-    const {data} = await axios.get(`${process.env.USER_SERVICE_URL}/api/v1/user/${blog[0].author}`);
 
-    const responseData = {message:"Blog fetched successfully", blog: blog[0], author: data}
+    // --------------------------
+    // FETCH AUTHOR FROM USER SERVICE
+    // --------------------------
+    const { data } = await axios.get(`${process.env.USER_SERVICE_URL}/api/v1/user/${blog[0].author}`);
 
+    const responseData = { success: true, message: "Blog fetched successfully", blog: blog[0], author: data }
+
+    // --------------------------
+    // SAVE TO CACHE FOR 1 HOUR
+    // --------------------------
     await setCahce(cacheKey, responseData, 3600)
 
     res.status(200).json(responseData);
